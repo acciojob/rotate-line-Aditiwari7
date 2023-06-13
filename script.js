666efe8e868c08e847b9827ba1e9e3929fa21c54
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-06-13 15:39:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let angle = 0;
+const speed = 1;
+
+function rotateLine() {
+	const line = document.getElementById('line');
+	angle += speed;
+	line.style.transform = `rotate(${angle}deg)`;
+	requestAnimationFrame(rotateLine);
+}
+
+rotateLine();
